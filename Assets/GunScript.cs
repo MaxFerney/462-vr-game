ffb846ec1a7d7a47187daf4dce3cb54703475475
Max Ferney <maxferney@gmail.com>
2021-04-15 16:02:14 -0400

    muzzle flash, on hit effects, particle systems.

--- Assets/GunScript.cs
+++ Assets/GunScript.cs
@@ -14,12 +14,21 @@ public class GunScript : MonoBehaviour
 
     //reference to Object
     public GameObject Gun;
+    
 
     //other vars
     public float damage = 10f;
+    public float fireRate = 15f;
     public float range = 100f;
+    public float impactForce = 30f;
+    
 
     public Camera gunTip;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+    public GameObject impaceOnStone;
+
+    private float nextTimeToFire = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,14 +36,18 @@ public class GunScript : MonoBehaviour
         FireShot.AddOnStateUpListener(TriggerUp, handType);
     }
     public void TriggerUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource){
-        Debug.Log("Trigger is Up");
+        //Debug.Log("Trigger is Up");
         Gun.GetComponent<MeshRenderer>().enabled = true;
     }
     public void TriggerDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
     {
-        Debug.Log("Trigger is Down");
+        //Debug.Log("Trigger is Down");
         //Gun.GetComponent<MeshRenderer>().enabled = false;
-        Shoot();
+        if (Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f/fireRate;
+            Shoot();
+        }
     }
 
     // Update is called once per frame
@@ -45,6 +58,7 @@ public class GunScript : MonoBehaviour
 
     void Shoot()
     {
+        muzzleFlash.Play();
         RaycastHit hit;
         
         if (Physics.Raycast(gunTip.transform.position, gunTip.transform.forward, out hit, range))
@@ -52,11 +66,26 @@ public class GunScript : MonoBehaviour
             Debug.Log(hit.transform.name);
 
             //Gun.GetComponent<MeshRenderer>().enabled = false;
-
+            
             Target target = hit.transform.GetComponent<Target>();
             if (target != null){
                 target.takeDamage(damage);
             }
+
+            isStoneMat stoneTarget = hit.transform.GetComponent<isStoneMat>();
+            if (stoneTarget != null){
+                GameObject stoneImpactGO = Instantiate(impaceOnStone, hit.point, Quaternion.LookRotation(hit.normal));
+                Destroy(stoneImpactGO, 5f);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 1f);
         }
     }
 }
