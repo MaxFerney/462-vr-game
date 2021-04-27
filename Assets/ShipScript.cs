2269c6df1a1ed68371c73c7edd73b01b3bf18c89
Max Ferney <maxferney@gmail.com>
2021-04-27 14:04:16 -0400

    fixed ship movement, added ship death.
    ship movement is now properly dynamic. movement is based on ship spawn pos and ship fall pos. once looses health, currently, just destroys that object and creates dead ship object.

--- Assets/ShipScript.cs
+++ Assets/ShipScript.cs
@@ -4,36 +4,52 @@ using UnityEngine;
 
 public class ShipScript : MonoBehaviour
 {
-    public float MaxMoveDistance = 10f;
+    
     public float MaxHealth = 100f;
     private float health;
-    //public GameObject ShipToTransform;
-    public GameObject ShipMesh;
+    private float moveConstant;
+    //currently this isnt doing anything.
+    //public GameObject ShipMesh;
 
     public Transform ShipSpawnPoint;
-    public Transform ShipPos;
+    public GameObject Ship;
+    public Transform FallPos;
+    public GameObject ShipDeathObject;
     // Start is called before the first frame update
     void Start()
     {
         health = MaxHealth;
+        moveConstant = FallPos.position.z-ShipSpawnPoint.position.z;
     }
     public void damageShip (float amount)
     {
         health -= amount;
-        var newZ = (MaxHealth - health) * (1f / MaxMoveDistance) ; //0-10 (max move distance)
-        
+        // doing MaxHealth in case health is added back to ship.
+        var newZ = ( 1f - (health/MaxHealth)) * moveConstant ; //moves ship based on health and relative to fall position.
+        //Debug.Log(newZ);
         if (health <= 0f)
         {
             Die();
         }
         else
         {
-            ShipPos.position = new Vector3(ShipSpawnPoint.position.x, ShipSpawnPoint.position.y, (ShipSpawnPoint.position.z + newZ) );
+            var dividedZ = (ShipSpawnPoint.position.z + newZ);
+            //Debug.Log(dividedZ);
+            Ship.transform.position = new Vector3(ShipSpawnPoint.position.x, ShipSpawnPoint.position.y, dividedZ );
         }
     }
     void Die()
     {
-        ShipPos.position = new Vector3(ShipPos.position.x, ShipPos.position.y, ShipSpawnPoint.position.z + MaxMoveDistance);
+        //destroy original object
+        Destroy(Ship);
+
+        //show dead ship mesh
+        MeshRenderer DeadShipMesh = ShipDeathObject.GetComponent<MeshRenderer>();
+        DeadShipMesh.enabled = true;
+
+        //show parts of dead ship (turn child object on, and their subObjects)
+        helperScripts.SetActiveAllChildren(ShipDeathObject.transform, true);
         //Destroy(ShipMesh);
     }
+    
 }
---
+++ Assets/helperScripts.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helperScripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public static void SetActiveAllChildren(Transform transform, bool value)
+    {
+        foreach(Transform child in transform)
+        {
+            child.gameObject.SetActive(value);
+
+            SetActiveAllChildren(child, value);
+        }
+    }
+
+}
