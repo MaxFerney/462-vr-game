ab02311bc99f2ed857e0784225d57042a4f5f61a
Max Ferney <maxferney@gmail.com>
2021-04-28 17:00:08 -0400

    Start in a menu room, push button, then tp to game area.

---
+++ Assets/StartGameButtonScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGameButtonScript : MonoBehaviour
+{
+    public Transform player;
+    public Transform newSpawn;
+    public GameObject gunObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartGameFromMenu()
+    {
+        player.position = new Vector3(newSpawn.position.x, newSpawn.position.y, newSpawn.position.z);
+        //player.rotation = newSpawn.rotation;
+        //player.localScale = newSpawn.localScale;
+        gunObject.SetActive(true);
+    }
+}
--- Assets/change_scene.cs
+++ Assets/change_scene.cs
@@ -2,13 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Valve.VR;
 
 
 public class change_scene : MonoBehaviour
 {
    
-    public void LoadScene(string scene_name){
+    public void LoadScene(string scene_name)
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(scene_name);
+        
+        // Debug.Log(scene_name);
+        // AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene_name);
+        // while (!asyncLoad.isDone)
+        // {
+        //     //do loading stuff on screen
+        //     Debug.Log("loading");
+        // }
+        // if (asyncLoad.isDone)
+        // {
+        //     SceneManager.SetActiveScene(SceneManager.GetSceneByName(scene_name));
+        //     AsyncOperation asyncUnload = SceneManager.UnloadSceneAsync(currentScene.name);
+        //     if (asyncUnload.isDone)
+        //     {
+        //         Debug.Log("unloaded scene successfully");
+        //         //Resources.UnloadUnusedAssets();
+        //     }
+        // }
+        
+
     }
 
 }
