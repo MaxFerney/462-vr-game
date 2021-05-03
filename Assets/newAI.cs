678f87d6207913fd4f94e80119499d04710adcb2
Michael Walters <waltersm8@winthrop.edu>
2021-05-03 11:42:59 -0400

    Smaller AI

---
+++ Assets/newAI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newAI : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject current = new GameObject();
+        }
+    }
+}
